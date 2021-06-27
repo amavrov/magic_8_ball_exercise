a83c740f69e52c3f938a2d3176c4ce2e9d9eaa4a
amavrov <amavrov@students.softuni.bg>
2021-06-28 00:27:52 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text('Ask me anything'),
+          foregroundColor: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int ballState = 1;
+
+  void shakeBall() {
+    setState(() {
+      ballState = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: TextButton(
+            child: Image.asset('ball$ballState'),
+            onPressed: () {},
+          ),
+        ),
+      ],
+    );
+  }
+}
